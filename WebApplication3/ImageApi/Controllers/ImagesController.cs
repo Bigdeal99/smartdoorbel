d3a70f36d34fc93d836d0c3d7cb5aa6f18ca6b66
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2024-05-28 16:36:51 +0200

    d

--- WebApplication3/ImageApi/Controllers/ImagesController.cs
+++ WebApplication3/ImageApi/Controllers/ImagesController.cs
@@ -1,17 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Azure.Storage.Blobs;
+using MQTTnet;
+using MQTTnet.Client;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System;
+
 
 [Route("api/[controller]")]
 [ApiController]
 public class ImagesController : ControllerBase
 {
     private readonly BlobServiceClient _blobServiceClient;
+    private readonly IMqttClient _mqttClient;
 
     public ImagesController(BlobServiceClient blobServiceClient)
     {
         _blobServiceClient = blobServiceClient;
+        
+        // Configure MQTT client options
+        var options = new MqttClientOptionsBuilder()
+            .WithClientId("API_Client")
+            .WithTcpServer("mqtt.flespi.io", 1883)
+            .WithCredentials("YOUR_FLESPI_TOKEN_FOR_API", "")
+            .WithCleanSession()
+            .Build();
+
+        // Create and connect MQTT client
+        _mqttClient = new MqttFactory().CreateMqttClient();
+        _mqttClient.ConnectAsync(options).Wait();
     }
 
     public class ImageDto
@@ -48,5 +66,40 @@ public class ImagesController : ControllerBase
         }
 
         return NotFound();
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadImage([FromBody] byte[] imageData)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient("iot-10sec-video");
+
+        var fileName = $"{DateTime.UtcNow:yyyyMMddHHmmss}.jpg";
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        await blobClient.UploadAsync(new BinaryData(imageData));
+
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("iot/notification")
+            .WithPayload("Image uploaded")
+            .Build();
+
+        await _mqttClient.PublishAsync(message);
+
+        await SendIFTTTNotification();
+
+        return Ok(new { Url = blobClient.Uri.ToString(), FileName = fileName });
+    }
+
+    private async Task SendIFTTTNotification()
+    {
+        using (var client = new HttpClient())
+        {
+            var requestUri = "https://maker.ifttt.com/trigger/button_pressed/with/key/nYTXhTPJg0LkIgtxP45lX8OmITfAhV_3zEN7LGMCTEz";
+            var response = await client.GetAsync(requestUri);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Error sending IFTTT notification: {response.StatusCode}");
+            }
+        }
     }
 }
