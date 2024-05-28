5b2b6b2144ac48a7da68b72173c7601de1552c1c
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2024-05-29 01:37:26 +0200

    Update Program.cs

--- WebApplication3/WebApplication3/Program.cs
+++ WebApplication3/WebApplication3/Program.cs
@@ -8,11 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configure services
 builder.Services.AddSingleton<IAzureBlobService, AzureBlobService>();
-builder.WebHost.UseUrls("http://*:9999");
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8181";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 builder.Services.AddSingleton<WebSocketServerManager>(sp =>
-    new WebSocketServerManager("ws://0.0.0.0:"+port, builder.Configuration.GetConnectionString("DefaultConnection")));
+    new WebSocketServerManager($"ws://0.0.0.0:{port}", builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add controllers
 builder.Services.AddControllers();
@@ -31,8 +32,8 @@ builder.Services.AddCors(options =>
 // Configure Kestrel
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(5164);
-    serverOptions.ListenAnyIP(7026, listenOptions => listenOptions.UseHttps());
+    serverOptions.ListenAnyIP(int.Parse(port));
+    // Removed HTTPS configuration
 });
 
 var app = builder.Build();
